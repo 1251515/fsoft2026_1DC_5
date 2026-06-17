cfff0d0dded8d9b20d4737bb00a93cc9ecbae7c2
1230748 <1230748@isep.ipp.pt>
2026-06-17 16:59:16 +0100

    main

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,3 +1,35 @@
-//
-// Created by bruno on 16/06/2026.
-//
+#include "../../headers/controller/Controller.h"
+#include "../../headers/model/GestorClientes.h"
+#include "../../headers/model/GestorProdutos.h"
+#include "../../headers/model/GestorEmpregados.h"
+#include "../../headers/model/GestorVendas.h"
+
+using namespace std;
+using namespace loja::controller;
+using namespace loja::model;
+
+int main() {
+
+    GestorClientes* gestorClientes = new GestorClientes();
+    GestorProdutos* gestorProdutos = new GestorProdutos();
+    GestorEmpregados* gestorEmpregados = new GestorEmpregados();
+    GestorVendas* gestorVendas = new GestorVendas();
+
+    Controller controller(
+        gestorClientes,
+        gestorProdutos,
+        gestorEmpregados,
+        gestorVendas
+    );
+
+    controller.exec();
+
+    cout << "Application has exited" << endl;
+
+    delete gestorClientes;
+    delete gestorProdutos;
+    delete gestorEmpregados;
+    delete gestorVendas;
+
+    return 0;
+}
